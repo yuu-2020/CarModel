1d6d8b2fccce07474cd5273d4ee607c92b331614
yuu-2020 <yuu.developper.2020@gmail.com>
2019-12-19 20:09:21 +0900

    Initial Commit

---
+++ CarModel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarModel
+//
+//  Created by Yudai Asano on 2019/12/19.
+//  Copyright © 2019 Yudai Asano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
